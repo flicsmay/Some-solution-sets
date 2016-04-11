336d4186f587a145b907e0af08a3080f8b9e4c07
fengye lee <flicsmay@163.com>
2016-04-11 19:54:23 +0800

    update to 8.23

---
+++ CSAPP/chapter 8/8.22.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int mysystem(char *command)
+{
+    int status;
+    char *argv[] = {"sh", "-c", command};
+    if (!fork()) {
+        // execve has been call and never return 
+        execve("/bin/sh", argv, NULL);
+        return -1; // if fail to call execve
+    }
+
+    if (waitpid(-1, &status, 0) > 0)
+        if (WIFEXITED(status) != 0) // if have been sucessful return
+            return WEXITSTATUS(status); // return exit status
+        else
+            return status; 
+    return -1;
+}
+
+
+int main(int argc, char *argv[])
+{
+   printf("%d\n",  mysystem(argv[1]));
+}
